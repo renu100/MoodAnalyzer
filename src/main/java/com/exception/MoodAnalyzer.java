a342b42499b31f76f0c97a4fac2fe87f4828b14f
renu100 <renu.chandraker08@gmail.com>
2021-10-15 23:37:28 +0530

    Added: Repeat analyse mood test case for sad

--- src/main/java/com/exception/MoodAnalyzer.java
+++ src/main/java/com/exception/MoodAnalyzer.java
@@ -1,11 +1,17 @@
 package com.exception;
 
 public class MoodAnalyzer {
+	private String message;
+
 	public MoodAnalyzer() {
 	}
 
-	public String analyzerMood(String message) {
-		if (message.contains("Sad"))
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	public String analyseMood() {
+		if (this.message.contains("Sad"))
 			return "SAD";
 		else
 			return "HAPPY";
